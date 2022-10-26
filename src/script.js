7f9ab932a7a70052dfa1cc65701f4a338bebff66
setyawannn <prayogasetyawan.adi@gmail.com>
2022-10-26 10:25:47 +0700

    Add: fullscreen btn

--- src/script.js
+++ src/script.js
@@ -8,6 +8,31 @@ function displayTime() {
   document.getElementById('minutes').setAttribute("style", `--value:${min}`);
   document.getElementById('seconds').setAttribute("style", `--value:${sec}`);
 }
-
-
 setInterval(displayTime, 10);
+
+// Full Screen
+
+function getFullscreenElement() {
+  return document.fullscreenElement
+    || document.webkitFullScreenElement
+    || document.mozFullScreenElement
+    || document.msFullScreenElement;
+}
+
+function toggleFullscreen() {
+  if (getFullscreenElement()) {
+    document.exitFullscreen;
+  } else {
+    document.getElementById('myClock').requestFullscreen().catch(console.log);
+  }
+}
+
+const btnFullscreen = document.getElementById('enterFullscreen');
+
+btnFullscreen.addEventListener("click", () => {
+  toggleFullscreen();
+});
+
+document.addEventListener("fullscreenchange", () => {
+  console.log("Full screen changed!");
+});
